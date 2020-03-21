d3a3082edfeadace9d86a5c12632961f14c02637
uuu0614 <33905956+uuu0614@users.noreply.github.com>
2020-03-21 10:18:51 +0900

    トークンの読み込み処理を追加

--- run.rb
+++ run.rb
@@ -5,3 +5,20 @@ require "discordrb"
 require "./my_library/emoji_bar.rb"
 require "./my_library/othello.rb"
 require "./my_library/savedata_v2.rb"
+
+#Botトークン取得処理。
+
+tokens = YAML.load_file("./config/tokens.yml")
+
+if tokens["using"]
+    token_key = tokens["using"]
+else
+    raise "The token to be used has not been specified.  使用するトークンが指定されていません。"
+end
+
+token = nil
+if tokens[token_key]
+    token = tokens[token_key]
+else
+    raise "The token name \"#{token_key}\" does not exist in config / tokens.yml.トークン名 「#{token_key}」 は config/tokens.yml に存在しません。"
+end
